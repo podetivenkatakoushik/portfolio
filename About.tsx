6980bafa59010b574ca91e0d61d0c92283cae329
podetivenkatakoushik <koushikpodeti@gmail.com>
2026-02-10 19:58:00 +0530

    Update About.tsx

--- About.tsx
+++ About.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { QUALITIES } from '../constants';
+import { QUALITIES } from './constants';
 
 const About: React.FC = () => {
   return (
