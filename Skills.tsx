0d5a9f00bec0983d5622773aa105ba80ab44727e
podetivenkatakoushik <koushikpodeti@gmail.com>
2026-02-10 19:56:38 +0530

    Update Skills.tsx

--- Skills.tsx
+++ Skills.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { SKILL_CATEGORIES } from '../constants';
+import { SKILL_CATEGORIES } from './constants';
 
 const Skills: React.FC = () => {
   return (
